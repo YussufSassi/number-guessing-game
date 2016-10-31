07c22a61d7f4ae23fe9a70e59addc9a4e2e0bdcb
YussufSassi <yussufsassi@gmail.com>
2016-10-31 21:33:00 +0100

    Changed some Strings

--- numberguessing.py
+++ numberguessing.py
@@ -3,9 +3,9 @@ number = randint (0,50)
 while True:
     user_number = int(input("please enter a number between 0 and 50 : "))
     if user_number < number :
-        print("you're a fucking idiot it's bigger than " + str(user_number))
+        print("It's bigger than " + str(user_number))
     elif user_number > number :
-        print("you're a fucking idiot it's smaller than " + str(user_number))
+        print("It's smaller than " + str(user_number))
     else:
-        print("God finally that's the right number")
+        print("Congratiulations that's the right number")
         break
